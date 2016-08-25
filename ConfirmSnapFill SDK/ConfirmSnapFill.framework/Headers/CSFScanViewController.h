c57916dbd4111f115ef8caa6763b160077ba5131
Greg <codecamel@gmail.com>
2016-08-25 11:44:54 -0700

    Update Build/Pod

--- ConfirmSnapFill SDK/ConfirmSnapFill.framework/Headers/CSFScanViewController.h
+++ ConfirmSnapFill SDK/ConfirmSnapFill.framework/Headers/CSFScanViewController.h
@@ -8,7 +8,6 @@
 
 @property (nonatomic) id<CSFScannerDelegate> delegate;
 @property (nonatomic, strong) IBOutlet CSFOverlayView *overlayView;
-@property (strong, nonatomic) IBOutlet UIView *view;
 
 //! @warn preferred instantiating factory call
 + (CSFScanViewController*)controller;
